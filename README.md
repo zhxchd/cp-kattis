0c7d177c90b30cdc1691cc74f2b81d835a58255f
Zhu Xiaochen <zhuxiaochen1005@gmail.com>
2021-05-10 18:28:30 +0800

    Complete bryr

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Newly-solved problems are listed on the top of the table.
 
 | | Problem | Difficulty | Status | Solution |
 |-|-|-|-|-|
-| 7 | [Bridges](./problems/bryr) | 1.4 - 2.6 | Working | [Java](./problems/bryr/Main.java)|
+| 7 | [Bridges](./problems/bryr) | 1.4 - 2.6 | Accepted (100) | [Java](./problems/bryr/Main.java)|
 | 6 | [Birthday Memorization](./problems/fodelsedagsmemorisering) | 1.3 - 1.7 | Accepted (100) | [Java](./problems/fodelsedagsmemorisering/Main.java)|
 | 5 | [Arrangement](./problems/upprodun) | 1.3 - 1.8 | Accepted (100) | [Java](./problems/upprodun/Main.java)|
 | 4 | [Homework](./problems/heimavinna) | 1.1 - 1.4 | Accepted (100) | [Java](./problems/heimavinna/Main.java)|
--- problems/bryr/Main.java
+++ problems/bryr/Main.java
@@ -4,8 +4,54 @@ import java.io.*;
 class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
-        
+        st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        Map<Integer, Integer> c = new HashMap<>();
+        int[] d = new int[v];
+        boolean[] visit = new boolean[v];
+        boolean[] inq = new boolean[v];
+        List<Integer>[] adj = new List[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+            d[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken()) - 1;
+            int v2 = Integer.parseInt(st.nextToken()) - 1;
+            int a = Integer.parseInt(st.nextToken());
+            adj[v1].add(v2);
+            adj[v2].add(v1);
+            c.put((v1 + v2) * (v1 + v2 + 1) / 2 + v2, a);
+            c.put((v1 + v2) * (v1 + v2 + 1) / 2 + v1, a);
+        }
+        PriorityQueue<Integer> q = new PriorityQueue<>((v1, v2) -> Integer.compare(d[v1], d[v2]));
+        q.offer(0);
+        d[0] = 0;
+        inq[0] = true;
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            visit[x] = true;
+            if (x == v - 1) break;
+            for (int y : adj[x]) {
+                if (!visit[y]) {
+                    int temp = c.get((x + y + 1) * (x + y) / 2 + y) + d[x];
+                    if (temp < d[y]) {
+                        d[y] = temp;
+                        if (inq[y]) q.remove(y);
+                    }
+                    q.offer(y);
+                    inq[y] = true;
+                }
+            }
+        }
+        bw.write(String.valueOf(d[v - 1]));
+        bw.write("\n");
+        bw.close();
     }
 }
